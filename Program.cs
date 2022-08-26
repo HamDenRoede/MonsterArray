0b70b6b448f9a0a0585bc019be85450157e43a87
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-26 11:01:49 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test();
+            //MainMenu();
+            Monster();
+
+        }
+
+        static void Test()
+        {
+            string input = "";
+            File.WriteAllText(@"c:\Users\Jonas\source\repos\MonsterArray\MonsterArray\data_files\drengenavn.dat", input, Encoding.Unicode);
+        }
+
+        static void MainMenu()
+        {
+            string ord = "Min hjerne fungere slet ikke nu yyy";
+            string[] Ord_array = ord.Split(' ');
+
+            Console.Write(Ord_array.Length);
+            Console.ReadKey();
+
+
+        }
+
+        static void Monster()
+        {
+
+            string[] question = { "fornavn", "efternavn", "vej", "by", };
+            string[] answer = new string[4];
+
+            for (int i = 0; i < question.Length; i++)
+            {
+                Console.WriteLine(question[i]);
+                answer[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine("");
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+
+            string input = "";
+
+            for (int i = 0; i < answer.Length; i++) ;
+            {
+
+            }
+
+            Console.ReadKey();
+            
+            File.WriteAllText(@"c:\Users\Jonas\source\repos\MonsterArray\MonsterArray\data_files\test_file.dat", input, Encoding.Unicode);
+
+        }
+    }
+}
